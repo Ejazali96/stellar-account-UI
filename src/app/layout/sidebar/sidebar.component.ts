a4f6fbfcee3275783987cd9273884cbfbcbf5c34
Abdul Qayyum <qayyumgb96@gmail.com>
2025-12-20 11:55:23 +0500

    sidebar design updated

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { NavigationItem } from '../../core/models/navigation.model';
 import { User } from '../../core/models/user.model';
 
@@ -9,12 +10,45 @@ import { User } from '../../core/models/user.model';
     standalone: true,
     imports: [CommonModule, RouterModule],
     templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.scss']
+    styleUrls: ['./sidebar.component.scss'],
+    animations: [
+        trigger('expandCollapse', [
+            state('collapsed', style({
+                height: '0',
+                opacity: '0',
+                overflow: 'hidden'
+            })),
+            state('expanded', style({
+                height: '*',
+                opacity: '1',
+                overflow: 'visible'
+            })),
+            transition('collapsed <=> expanded', [
+                animate('300ms cubic-bezier(0.4, 0, 0.2, 1)')
+            ])
+        ])
+    ]
 })
 export class SidebarComponent {
     @Input() navigationItems: NavigationItem[] = [];
     @Input() user: User | null = null;
     @Output() signOut = new EventEmitter<void>();
+
+    isCollapsed = false;
+    isInfoBoxExpanded = false;
+
+    toggleSidebar(): void {
+        this.isCollapsed = !this.isCollapsed;
+        if (this.isCollapsed) {
+            this.isInfoBoxExpanded = false;
+        }
+    }
+
+    toggleInfoBox(): void {
+        if (!this.isCollapsed) {
+            this.isInfoBoxExpanded = !this.isInfoBoxExpanded;
+        }
+    }
 
     onSignOut(): void {
         this.signOut.emit();
